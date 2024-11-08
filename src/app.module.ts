1c2a9973470c58b9033a2ce9804a3bdf4dbf0877
mezba-dev <csmezba@gmail.com>
2024-11-08 13:37:35 +0600

    add swagger

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { VideoGateway } from './video/video.gateway';
+import { DocModule } from './doc/doc.module';
 
 @Module({
   providers: [VideoGateway],
+  imports: [DocModule],
 })
 export class AppModule {}
